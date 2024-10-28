5cc0bdaa624b99f774e17ae4f6b090e4769027a2
Marcos <marcos.attwell@gmail.com>
2024-10-27 23:09:06 -0300

    experimentos de power method y arreglos

--- film-classifier/variables.py
+++ film-classifier/variables.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 genre_name = [
     "crime",
     "romance",
@@ -16,3 +18,17 @@ def print_avance(i, j, iMax, jMax):
         print(int(porcentaje / total * 100), "%", end="\r", flush=True)
     if (i + 1) * (j + 1) == total:
         print("100 %!")
+
+
+def matrizHouseholder(D, v):
+    """Devuelve matriz Householder I (forma de D) - 2 v vt"""
+    "v vector unitario"
+    return np.eye(D.shape[0]) - 2 * (v @ v.T)
+
+
+def trucoHouseholder(D):
+    """D debe ser una matriz diagonal"""
+    v = np.random.rand(D.shape[0], 1)
+    v = v / np.linalg.norm(v)
+    H = matrizHouseholder(D, v)
+    return H @ D @ H.T
